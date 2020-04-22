c1e290fca72651f91f540b2892dbaef49dcf5713
David Lewis <david.lewis@computershare.co.uk>
2020-04-22 09:23:58 +0100

    Added someClass

---
+++ someClass/someClass.ts
@@ -0,0 +1,9 @@
+class SomeClass {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    method() {
+        return "Hello, " + this.greeting;
+    }
+}
